609f224b9385ba881164ec26af9d61938ddfdbbd
Joseph Wangemann and Tracy Jo Reith <joseph@>
2019-01-23 14:38:26 -0800

    Fix code to replace vowels with dashes and display word puzzle and toggle between sentence input and output

--- js/scripts.js
+++ js/scripts.js
@@ -4,23 +4,30 @@ $(document).ready(function(){
 
     var inputSentence = $("#inputSentence").val();
     var newSentence = inputSentence.split("");
+    // var testInput = "I am not too crazy am i or else"
+    // var newSentence = testInput.split("");
+    var replacedVowels = [];
+    var puzzleSentence = [];
+    newSentence.forEach(function(letter){
+      console.log(letter);
+      if ((letter === 'a') || (letter === 'e') || (letter === 'i' ) || (letter === 'o' ) || (letter === 'u' )) {
+        replacedVowels.push('-');
+      } else {
+        replacedVowels.push(letter);
+      }
+    });
+
+    console.log(replacedVowels);
     console.log(newSentence);
+    console.log(replacedVowels);
 
-    // var vowels = ['a', 'e', 'i', 'o', 'u'];
-    // console.log(vowels);
-    //
-    // vowels.forEach(function(vowel){
-    //   return vowel;
-      var vowels = [];
-      newSentence.map(function(letter){
-        var puzzleSentence = letter.replace('a', '-');
-        vowels.push(puzzleSentence);
-
-        console.log(vowels);
-        var output = vowels.join("");
-        $(".output").show();
-        $(".word-puzzle").text(output);
-      });
-    // })
+      var output = replacedVowels.join("");
+      $(".container").children().toggle();
+      $(".word-puzzle").text(output);
   });
+
+  $("button#redo").click(function(){
+  setTimeout(function () {
+   location.reload()}, 100);
+ });
 });
